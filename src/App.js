fc79d5e6a62351051d5e4e51712e7f8dcb981466
Jujubash <jdssvetter@gmail.com>
2023-02-13 14:17:53 +0100

    ADDED: test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
+import React from 'react';
 import logo from './Images/logo.svg';
 import wind from './Images/wind.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App-loadblock">
-          <img src={wind} className="App-load" alt="wind" />
-        </div>
-    {/* Deutsch */}
+/* DEUTSCH
         <div className="DEmain">
           <a
             className="App-link"
@@ -21,8 +14,8 @@ function App() {
             Deutsch
           </a>
         </div>
-    {/* English */}     
-        <div className="ENmain">
+ English 
+     <div className="ENmain">
           <a
             className="App-link"
             href="http://www.wikipedia.org/"
@@ -32,38 +25,47 @@ function App() {
             English
           </a>
         </div>
-    {/* Português */}     
-        <div className="PTmain">
-          <a
-            className="App-link"
-            href="http://www.wikipedia.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Português
-          </a>
-        </div>
-    {/* Japanese */}
-        <div className="JPmain">
-          <a
-            className="App-link"
-            href="http://www.wikipedia.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Japanese
-          </a>
-        </div>
-    {/* Español */}
-        <div className="SPmain">
-          <a
-            className="App-link"
-            href="http://www.wikipedia.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Español
-          </a>
+Português 
+    <div className="PTmain">
+    <a
+      className="App-link"
+      href="http://www.wikipedia.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Português
+    </a>
+  </div>
+ Japanese 
+  <div className="JPmain">
+    <a
+      className="App-link"
+      href="http://www.wikipedia.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Japanese
+    </a>
+  </div>
+ Español 
+  <div className="SPmain">
+    <a
+      className="App-link"
+      href="http://www.wikipedia.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Español
+    </a>
+  </div> */
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-loadblock">
+          <img src={wind} className="App-load" alt="wind" />
         </div>
       </header>
     </div>
