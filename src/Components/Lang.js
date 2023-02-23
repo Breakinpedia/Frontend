80a08be07c706ea8111ae2945c51de73aaf4711c
Jujubash <jdssvetter@gmail.com>
2023-02-23 17:18:02 +0100

    DONE: language squares

--- src/Components/Lang.js
+++ src/Components/Lang.js
@@ -1,28 +1,27 @@
 import React from "react";
 
+// de en es fr it nl pt ar fa hi ja ko ru zh
 export default function Logo(){
-    var items = document.querySelectorAll( '.grid__item' )
-    var count = 0;
-    for( var y = 1; y <= 8; y++ )
-    {   
-        for( var x = 1; x <= 8; x++ )
-        {
-            var item = items[ count ]
-            item.style['-ms-grid-column'] = x
-            item.style['-ms-grid-row'] = y
-            count++
-        }
-    }
     return(
         <div id="lang-rectan" class="lang" >
-            <div id="jp-square" class="square"> </div>
-            <div id="pt-square" class="square"> </div>
-            <div id="de-square" class="square"> </div>
-            <div id="en-square" class="square"> </div>
-            <div id="sp-square" class="square"> </div>
-            <div id="it-square" class="square"> </div>
-            <div id="ko-square" class="square"> </div>
-            <div id="fr-square" class="square"> </div>
+            {/* LANGS BEFORE LOGO */}
+            <div id="de-square" class="square1">Deutsch</div>
+            <div id="en-square" class="square2">English</div>
+            <div id="sp-square" class="square1">Español</div>
+            <div id="fr-square" class="square2">Français</div>
+            <div id="it-square" class="square1">Italiano</div>
+            <div id="nl-square" class="square2">Nederlands</div>
+            <div id="pt-square" class="square1">Português</div>
+            {/* MIDDLE: LOGO */}
+            <div id="ALL-square" class="square2"> LOGO </div>
+            {/* LANGS AFTER LOGO */}
+            <div id="ar-square" class="square1">Arabic</div>
+            <div id="fa-square" class="square2">Persian</div>
+            <div id="hi-square" class="square1">Hindi</div>
+            <div id="jp-square" class="square2">Japanese</div>
+            <div id="ko-square" class="square1">Korean</div>
+            <div id="ru-square" class="square2">Russian</div>
+            <div id="zh-square" class="square1">Chinese</div>
         </div>
     )
 }
